3c551dca754eca4b4194376dec0ec9dcba3d9a6c
Haryu5412 <haryu5412@gbsw.gyo6.net>
2024-11-28 08:48:44 +0900

    이름에 맞게 어플리케이션 이름 변경.

--- src/main/java/com/ada/foodclip/FoodcliveApplication.java
+++ src/main/java/com/ada/foodclip/FoodcliveApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FoodclipApplication {
+public class FoodcliveApplication {
     public static void main(String[] args) {
-        SpringApplication.run(FoodclipApplication.class, args);
+        SpringApplication.run(FoodcliveApplication.class, args);
         System.out.println("Server Open");
         System.out.println("로그인 테스트 : http://localhost:8080/auth/login");
         System.out.println("회원가입 테스트 : http://localhost:8080/auth/signup");
