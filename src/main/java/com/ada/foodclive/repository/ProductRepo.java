ee6e3e47cc1e292dfd88d70c0e0b7417c3bca209
Haryu5412 <haryu5412@gbsw.gyo6.net>
2024-12-03 00:28:40 +0900

    Product 주석 처리 추가

--- src/main/java/com/ada/foodclive/repository/ProductRepo.java
+++ src/main/java/com/ada/foodclive/repository/ProductRepo.java
@@ -4,4 +4,5 @@ import com.ada.foodclive.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepo extends JpaRepository<Product, Long> {
+    // Product 엔티티 관리 선언 예정
 }
