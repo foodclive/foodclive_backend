f4a237ee8608c581fad8b029447625dc21b50dbc
Haryu5412 <haryu5412@gbsw.gyo6.net>
2024-12-04 15:56:16 +0900

    Update UserService.java

--- src/main/java/com/ada/foodclive/service/UserService.java
+++ src/main/java/com/ada/foodclive/service/UserService.java
@@ -5,6 +5,8 @@ import com.ada.foodclive.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @RequiredArgsConstructor
 @Service
 public class UserService {
